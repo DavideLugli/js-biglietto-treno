23e43d7f78b188c0273a0c25d846600d4af8f17b
Davide Lugli <luglidavide.web@gmail.com>
2019-12-18 15:11:32 +0100

    Fine Commit JS (no style)

--- js/trainticket.js
+++ js/trainticket.js
@@ -6,13 +6,17 @@ var etaPasseggero = parseInt(prompt('Quanti anni hai?'));
 console.log(etaPasseggero);
 // prezzo biglietto (0.21euro/km)
 var prezzoBiglietto = 0.21 * lunghezzaViaggio;
-console.log(prezzoBiglietto);
+
 // sconto del 20% se minorenne
 if (etaPasseggero < 18) {
   prezzoBiglietto = prezzoBiglietto - ((prezzoBiglietto / 100) * 20);
   console.log(prezzoBiglietto);
-} else {
-  // prezzoBiglietto = 0.21 * lunghezzaViaggio;
 }
-
 // sconto 40% se over 65
+else if (etaPasseggero >= 65) {
+  prezzoBiglietto = prezzoBiglietto - ((prezzoBiglietto / 100) * 40);
+  console.log(prezzoBiglietto);
+} else {
+  var prezzoBiglietto = 0.21 * lunghezzaViaggio;
+  console.log(prezzoBiglietto);
+}
